176e4e559cf6a1deb06ee4aa3278f8362f74f412
Your Name <alessanrospedroso@outlook.com>
2024-05-03 08:25:50 -0300

    centralizando view

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {View,Text,StyleSheet} from 'react-native'
 
-export default function App() {
+//()
+export default function App(){
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Text>Sujeito Programador</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    backgroundColor:"#F3F3FF",
+    justifyContent:"center",
+    alignItems:"center",
+  }
+})
