a79d328f2082a6c741196908656840a987fdc7ec
Your Name <alessanrospedroso@outlook.com>
2025-01-07 17:56:04 -0300

    estilizando o header

--- src/pages/passwords/index.js
+++ src/pages/passwords/index.js
@@ -1,13 +1,13 @@
-import { View, Text, StyleSheet } from 'react-native'
-
+import { View, Text, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 export function Passwords() {
-    return (
-        <View>
-            <Text>Minhas senhas</Text>
-        </View>
-    )
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.header}>
+        <Text style={styles.title}>Minhas senhas</Text>
+      </View>
+    </SafeAreaView>
+  );
 }
 
-const styles = StyleSheet.create({
-    
-})
+const styles = StyleSheet.create({});
